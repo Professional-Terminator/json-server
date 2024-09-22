13a5674dc2d893eb55c94f7476704846642b8876
saqibOctave <sali@octavebytes.com>
2024-09-23 01:57:36 +0500

    minor change

--- api/server.js
+++ api/server.js
@@ -12,6 +12,17 @@ const db = JSON.parse(data);
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
+
+
+// Use middlewares
+// server.use(middlewares);
+
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}));
+
 // Enable CORS
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
@@ -19,15 +30,6 @@ server.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-
-// Use middlewares
-server.use(middlewares);
-
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}));
 
 server.use(router);
 server.listen(3000, () => {
