2cce87d93c71bef77e5a81aa3835e5ea0e226da8
saqibOctave <sali@octavebytes.com>
2024-09-23 21:33:52 +0500

    change Server.json file

--- api/server.js
+++ api/server.js
@@ -1,40 +1,31 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server');
-const cors = require('cors'); // Import cors
-const fs = require('fs');
-const path = require('path');
+const jsonServer = require('json-server')
 
-// Create a JSON Server instance
-const server = jsonServer.create();
-const filePath = path.join('db.json');
+const server = jsonServer.create()
+
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db);
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router(db)
 
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
+const middlewares = jsonServer.defaults()
 
-// Use middlewares
-server.use(middlewares);
-
+server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}));
-
-// Enable CORS
-// server.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-//   });
-
-server.use(router);
+}))
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
