2864fc1201dc6fa16f331ccac88eac287eed8ee8
saqibOctave <sali@octavebytes.com>
2024-09-23 22:32:06 +0500

    update server.js

--- api/server.js
+++ api/server.js
@@ -1,56 +1,30 @@
-// // See https://github.com/typicode/json-server#module
-// const jsonServer = require('json-server')
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-// const server = jsonServer.create()
+const server = jsonServer.create()
 
-// // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
-// // Comment out to allow write operations
-// // const router = jsonServer.router('db.json')
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-// const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults()
 
-// server.use(middlewares)
-// // Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-// server.use(router)
-// server.listen(3000, () => {
-//     console.log('JSON Server is running')
-// })
-
-// // Export the Server API
-// module.exports = server
-
-const jsonServer = require('json-server');
-
-const server = jsonServer.create();
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join('db.json');
-
-// Use the router with the db.json file to allow write operations
-const router = jsonServer.router(filePath);
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}));
-
+}))
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
