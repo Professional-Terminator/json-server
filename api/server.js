8baea1b77f09a4ee355c0a6ca150aace5bc0c1bd
saqibOctave <sali@octavebytes.com>
2024-09-23 23:06:05 +0500

    update server.js

--- api/server.js
+++ api/server.js
@@ -1,46 +1,21 @@
 
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-// const middlewares = jsonServer.defaults()
-// server.use(middlewares)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-// server.use(router)
-// server.listen(3000, () => {
-//     console.log('JSON Server is running')
-// })
+import { create, router as _router, defaults, rewriter } from 'json-server'
+const server = create()
+import { readFileSync } from 'fs'
+import { join } from 'path'
+const filePath = join('db.json')
+const data = readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = _router(db)
+const middlewares = defaults()
+server.use(middlewares)
+server.use(rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-// module.exports = server
-
-const jsonServer = require('json-server');
-const path = require('path');
-
-const server = jsonServer.create();
-
-// Enable both read and write operations
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
-
-// Use default middlewares for JSON Server
-server.use(jsonServer.defaults());
-
-// Add custom middleware (optional)
-// server.use((req, res, next) => {
-//     // Your custom middleware logic here
-//     next();
-// });
-
-server.use(router);
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`);   
-
-});
+export default server
