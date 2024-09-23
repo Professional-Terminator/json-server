71c7723c05f59dfdafcc609856d1791ae479e65b
saqibOctave <sali@octavebytes.com>
2024-09-23 22:48:06 +0500

    changeServer.js

--- api/server.js
+++ api/server.js
@@ -1,30 +1,52 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
 
-const server = jsonServer.create()
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+// const middlewares = jsonServer.defaults()
+// server.use(middlewares)
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+// server.use(router)
+// server.listen(3000, () => {
+//     console.log('JSON Server is running')
+// })
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// module.exports = server
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
 
-server.use(middlewares)
+// Define the path to your db.json
+const filePath = path.join(__dirname, 'db.json');
+
+// Use the router with the db.json file to allow write operations
+const router = jsonServer.router(filePath);
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// Custom rewriter rules
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+}));
+
+// Use the router to handle the requests
+server.use(router);
+
+// Start the server
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
